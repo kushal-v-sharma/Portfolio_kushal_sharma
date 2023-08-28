663f90e7442de092f425525ccafc2ed9c7264f32
kushal sharma <106602016+kushal-v-sharma@users.noreply.github.com>
2023-08-28 18:02:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      //  sticky navbar on scroll script  //
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+  
+      //  scroll-up button show/hide script  //
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
+    });
+  
+
+    // script.js
+const certificates = document.querySelectorAll('.certificate');
+const modal = document.getElementById('certificate-modal');
+const modalImage = document.getElementById('modal-image');
+const modalTitle = document.getElementById('modal-title');
+const closeBtn = document.querySelector('.close');
+
+certificates.forEach(certificate => {
+  certificate.addEventListener('click', () => {
+    const src = certificate.getAttribute('data-src');
+    const title = certificate.getAttribute('data-title');
+    modalImage.src = src;
+    modalTitle.textContent = title;
+    modal.style.display = 'block';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+    
+    
+
+    //  slide-up script  //
+  
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+      //  removing smooth scroll on slide-up button click  //
+      $("html").css("scrollBehavior", "auto");
+    });
+  
+    $(".navbar .menu li a").click(function () {
+      //  Smooth scroll on Menu Items click  //
+  
+      $("html").css("scrollBehavior", "smooth");
+    });
+  
+    //  Toggle Navbar  //
+  
+    $(".menu-btn").click(function () {
+      $(".navbar .menu").toggleClass("active");
+      $(".menu-btn i").toggleClass("active");
+    });
+  
+    //  Typing Text Animation  //
+  
+    var typed = new Typed(".typing", {
+      strings: [
+        "BTECH FINAL YEAR IT STUDENT",
+        "Fresher",
+        "BTECH FINAL YEAR IT STUDENT",
+        "Fresher",
+        "BTECH FINAL YEAR IT STUDENT"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  
+    var typed = new Typed(".typing-2", {
+      strings: [
+        "BTECH FINAL YEAR IT STUDENT",
+        "Fresher",
+        "BTECH FINAL YEAR IT STUDENT",
+        "Fresher",
+        "BTECH FINAL YEAR IT STUDENT"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  
+    //  Owl Carousel  //
+  
+    $(".carousel").owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        600: {
+          items: 2,
+          nav: false
+        },
+        1000: {
+          items: 3,
+          nav: false
+        }
+      }
+    });
+  });
